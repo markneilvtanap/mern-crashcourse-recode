551fe78bc63414f07934c91f4a69c65bb0aefe05
MarkNeil03-hub <MarkNeilTanapWorkEmail@gmail.com>
2025-10-29 21:51:18 +0800

    change something again

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Box, useColorModeValue } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
-import Navbar from "./components/NavBar";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
